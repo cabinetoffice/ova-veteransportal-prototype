582e8f03776a8bdca5e5706807b532deabfdaa6e
william-eyre <weyre96@gmail.com>
2024-02-23 14:45:57 +0000

    continued work on finder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,10 @@
 window.GOVUKPrototypeKit.documentReady(() => {
+    document.addEventListener("DOMContentLoaded", function(event) { 
+        var scrollpos = localStorage.getItem('scrollpos');
+        if (scrollpos) window.scrollTo(0, scrollpos);
+    });
 
+    window.onbeforeunload = function(e) {
+        localStorage.setItem('scrollpos', window.scrollY);
+    };
 })
